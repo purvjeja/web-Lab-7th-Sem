0e8d70421bf6b7c158c9b87bef780e61024b662e
purvjeja <jejapurv@gmail.com>
2021-01-15 20:14:19 +0530

    Database

--- Program10/Program10.php
+++ Program10/Program10.php
@@ -1,17 +1,20 @@
 <?php
-$connect = mysqli_connect("localhost","purv","","purv");
+$connect = mysqli_connect("localhost","root","","purv");
+
 if (mysqli_connect_errno())
 {
-echo "Failed to connect to MySQL: ". mysqli_connect_error();
+echo "Failed to connect to MySQL:-". mysqli_connect_error();
 }
+
 if(isset($_POST['submit']))
 {
 $usn=$_POST['usn'];
 $name=$_POST['name'];
 $email=$_POST['email'];
 $mob=$_POST['mob'];
-$s="insert into stud values(".$usn.",'$name','$email','$mob')";
+$s="insert into stud values('$usn','$name','$email',$mob)";
 $ins=mysqli_query($connect,$s);
+
 if($ins)
 {
 echo "
@@ -22,6 +25,7 @@ alert('Inserted Successfully');
 }
 else
 {
+echo mysqli_error($connect);
 echo "
 <script>
 alert('Error in insertion');
